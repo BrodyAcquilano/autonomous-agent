d5299620816c0c8059b94d9d3d7d78f9704ce47c
Brody Acquilano <brodya2621@gmail.com>
2026-08-27 03:30:59 -0400

    Update to background in console to remove animations and use a nice background image.

--- src/Pages/Console/Console.jsx
+++ src/Pages/Console/Console.jsx
@@ -1,7 +1,6 @@
 import ConsoleViewport from "./Components/ConsoleViewport/ConsoleViewport";
 
 import CommandShell from "./Components/CommandShell/CommandShell";
-import CommunicationArray from "./Components/CommunicationArray/CommunicationArray";
 import LightPanel from "./Components/LightPanel/LightPanel";
 import MessagePanel from "./Components/MessagePanel/MessagePanel";
 import RequestControlPanel from "./Components/RequestControlPanel/RequestControlPanel";
@@ -11,41 +10,6 @@ import "./Console.css";
 
 const ROUTER_MODEL_ID =
   "gpt-5.6-terra";
-
-
-const TERMINAL_MAN_BANNER = String.raw`
-////////////////////////////////////////////////////////////////////////////////
-
-#####  #####  ####   #   #  #####  #   #   ###   #      #   #   ###   #   #
-  #    #      #   #  ## ##    #    ##  #  #   #  #      ## ##  #   #  ##  #
-  #    ####   ####   # # #    #    # # #  #####  #      # # #  #####  # # #
-  #    #      #  #   #   #    #    #  ##  #   #  #      #   #  #   #  #  ##
-  #    #####  #   #  #   #  #####  #   #  #   #  #####  #   #  #   #  #   #
-
-////////////////////////////////////////////////////////////////////////////////
-`;
-
-
-const STAR_FIELD_ONE = String.raw`
-       *                       .                       *
-                .                         *
-   .                                      .                    *
-                           *
-              *                                      .
-                                                *
-     .                *              .
-`;
-
-
-const STAR_FIELD_TWO = String.raw`
-              .                              *
-      *                                              .
-                         .         *
-                                                   *
-   *                  .                                 .
-                                 *
-            .                           *
-`;
 
 
 function Console({
@@ -83,43 +47,6 @@ function Console({
             scale,
           }) => (
             <>
-              <div
-                className="retro-background"
-                aria-hidden="true"
-              >
-                <div className="deep-space-glow" />
-
-
-                <pre className="terminal-stars terminal-stars-one">
-                  {STAR_FIELD_ONE}
-                </pre>
-
-
-                <pre className="terminal-stars terminal-stars-two">
-                  {STAR_FIELD_TWO}
-                </pre>
-
-
-                <div className="scanlines" />
-
-
-                <div className="screen-vignette" />
-              </div>
-
-
-              <pre
-                className="terminal-man-banner"
-                aria-hidden="true"
-              >
-                {TERMINAL_MAN_BANNER}
-              </pre>
-
-
-              <section className="console-workspace">
-                <CommunicationArray />
-              </section>
-
-
               <LightPanel
                 systemStatus={
                   systemStatus
